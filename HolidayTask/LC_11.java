6f8f8582523c3393cf0d01c71fe856e986a1d099
Garcia <garciamessyf@gmail.com>
2026-03-23 22:39:01 +0530

    Added HolidayTask

---
+++ HolidayTask/LC_11.java
@@ -0,0 +1,25 @@
+package HolidayTask;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+
+            maxArea = Math.max(maxArea, area);
+
+            // move the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
